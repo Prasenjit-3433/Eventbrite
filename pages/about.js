6a14cfe0612d402e0455cb270dc4dfa15c0a9296
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-01-12 11:49:21 +0530

    initial structure done for Home, Events & About-Us page

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About Us Page</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, adipisci
+        nihil ab facilis nulla, sit iusto odit deleniti saepe atque repudiandae
+        est autem cupiditate laboriosam aspernatur perferendis. Nesciunt,
+        quisquam ut.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/events/index.js
@@ -0,0 +1,20 @@
+const About = () => {
+    return(
+        <div>
+        <a href="">
+          <img src="" alt="" />
+          <h2>Events in London</h2>
+        </a>
+        <a href="">
+          <img src="" alt="" />
+          <h2>Events in San Francisco</h2>
+        </a>
+        <a href="">
+          <img src="" alt="" />
+          <h2>Events in Barcelona</h2>
+        </a>
+        </div>
+    );
+}
+
+export default About;

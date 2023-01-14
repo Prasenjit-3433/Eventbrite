9bd7601c697bb52fafa4dde83121a37719b5027e
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-01-14 11:29:27 +0530

    styled Header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,13 +4,17 @@ import Link from "next/link";
 const Header = () => {
     return(
         <header>
-            <nav>
-            <Image src="" alt="" />
-
-            <Link href="/">Home</Link>
-            <Link href="/events">Events</Link>
-            <Link href="/about">About</Link>
-            </nav>
+            <div className="top-nav">
+                <Image src="/images/logo.png" width={50} height={50} alt="logo"/>
+                <nav>
+                    <ul>
+                        <li><Link href="/">Home</Link></li>
+                        <li><Link href="/events">Events</Link></li>
+                        <li><Link href="/about">About</Link></li>
+                    </ul>
+                </nav>
+            </div>
+            <h1 id="header-text">Discover Great Events or Create Your Own & Sell Tickets</h1>
       </header>
     );
 }

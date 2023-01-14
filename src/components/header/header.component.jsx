2d8708bfe71671b23b5797c66d69adec4a25e3c5
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-01-14 12:48:17 +0530

    styles for Home page

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,17 +4,19 @@ import Link from "next/link";
 const Header = () => {
     return(
         <header>
-            <div className="top-nav">
-                <Image src="/images/logo.png" width={50} height={50} alt="logo"/>
-                <nav>
-                    <ul>
-                        <li><Link href="/">Home</Link></li>
-                        <li><Link href="/events">Events</Link></li>
-                        <li><Link href="/about">About</Link></li>
-                    </ul>
-                </nav>
+            <div>
+                <div className="top-nav">
+                    <Image src="/images/logo.png" width={50} height={50} alt="logo"/>
+                    <nav>
+                        <ul>
+                            <li><Link href="/">Home</Link></li>
+                            <li><Link href="/events">Events</Link></li>
+                            <li><Link href="/about">About</Link></li>
+                        </ul>
+                    </nav>
+                </div>
+                <h1 id="header-text">Discover Great Events or Create Your Own & Sell Tickets</h1>
             </div>
-            <h1 id="header-text">Discover Great Events or Create Your Own & Sell Tickets</h1>
       </header>
     );
 }
--- src/components/home/home-page.component.jsx
+++ src/components/home/home-page.component.jsx
@@ -4,19 +4,23 @@ import Link from "next/link";
 
 const HomePage = ({ data }) => {
     return (
-        <>
+        <div className="home_body">
         {
           data.map((event) => {
             return(
-              <Link href={`/events/${event.id}`} key={event.id}>
-                  <Image src={event.image} alt={event.title} height={300} width={300}/>
+              <Link className="card" href={`/events/${event.id}`} key={event.id}>
+                <div className="card-image">
+                  <Image src={event.image} alt={event.title} height={300} width={450}/>
+                </div>
+                <div className="card-content">
                   <h2>{event.title}</h2>
                   <p>{event.description}</p>
+                </div>
               </Link>
             );
           })
         }
-      </>
+      </div>
     );
 }
 
